62ecacc6fd23dd424fb7e6bdd783cb04fc9699a4
javialvab97 <61209232+javialvab97@users.noreply.github.com>
2020-02-26 20:23:26 -0600

    Add files via upload

---
+++ Flake8_Con_Errores.py
@@ -0,0 +1,21 @@
+def Escal(mo):
+    r= True
+    k= 0
+    m=0
+   
+    while (m<len(mo)):
+
+        if (mo[m][k]>0):
+            m+=1
+            for j in range(m, len(mo)):
+                if (mo[j][k]!=0):
+                    r= False
+        else:
+            k+=1           
+                
+    return r
+
+
+mo = [[1,2,2],[0,7,9],[0,0,4]]
+print (mo)
+print (Escal(mo))
